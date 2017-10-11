7574266d4cdfb09e2f368ce9517258607bcc0349
Matt McNeil <matt.mcneil1984@gmail.com>
2017-10-11 03:31:34 +0000

    Fixed fancybox JS for Images, Updated mobile styles

--- content/_ui-graphics.php
+++ content/_ui-graphics.php
@@ -1,26 +1,26 @@
 <div id="container">					
 	<div id="row">
     <h1 class="text-center">Logos</h1>
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo1-bretonsmartek.jpg" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo1-bretonsmartek.jpg" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a>
     	<p class="text-center font-weight-bold">Breton SmarTek Logo</p>
     </div>
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo2-fireq.jpg" alt="FireQ-RVS Logo"><img src="/images/graphic-design/Logo2-fireq.jpg" class="wide center-block"/></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo2-fireq.jpg" alt="FireQ-RVS Logo"><img src="/images/graphic-design/Logo2-fireq.jpg" class="wide center-block"/></a>
     	<p class="text-center font-weight-bold">FireQ-RVS Logo</p>
     </div>
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo3-cit.png" alt="Customer In Touch Logo"><img src="/images/graphic-design/Logo3-cit.png" class="wide center-block"/></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo3-cit.png" alt="Customer In Touch Logo"><img src="/images/graphic-design/Logo3-cit.png" class="wide center-block"/></a>
     	<p class="text-center font-weight-bold">Customer In Touch Logo</p>
     </div>
     
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo4-ggm.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/Logo4-ggm.jpg" class="wide center-block" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo4-ggm.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/Logo4-ggm.jpg" class="wide center-block" /></a>
     	<p class="text-center font-weight-bold">Go Green Messaging Logo</p>
     </div>
     
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/businessCard.png" alt="Go Green Messaging Logo"><img src="/images/graphic-design/businessCard.png" class="wide center-block" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/businessCard.png" alt="Go Green Messaging Logo"><img src="/images/graphic-design/businessCard.png" class="wide center-block" /></a>
     	<p class="text-center font-weight-bold">FireQ-RVS Business Cards</p>
     </div>
     
@@ -28,20 +28,21 @@
     
     <h1 class="text-center">Music Promotion Flyers</h1>
     
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer1.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer1.jpg" class="tall center-block" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer1.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer1.jpg" class="tall center-block" /></a>
     	<p class="text-center font-weight-bold">Electronic Funk 1</p>
     </div>
     
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer2.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer2.jpg" class="tall center-block" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer2.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer2.jpg" class="tall center-block" /></a>
     	<p class="text-center font-weight-bold">Electronic Funk 2</p>
     </div>
     
-    <div class="col-xs-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer3.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer3.jpg" class="tall center-block" /></a><br />
+    <div class="col-xs-12 col-md-4">
+    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer3.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer3.jpg" class="tall center-block" /></a>
     	<p class="text-center font-weight-bold">Lucky 13</p>
     </div>
     
+    <div class="newline" style="height:150px; clear:both;"></div>
 	</div>
 </div>
--- master.php
+++ master.php
@@ -47,16 +47,16 @@
 		<!-- Graphics Page Includes -->
 		
     <!-- Add fancyBox -->
-    <link rel="stylesheet" href="js/fancybox/source/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
-    <script type="text/javascript" src="js/fancybox/source/jquery.fancybox.pack.js?v=2.1.4"></script>
+    <link rel="stylesheet" href="/js/fancybox/source/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
+    <script type="text/javascript" src="/js/fancybox/source/jquery.fancybox.pack.js?v=2.1.4"></script>
     
     <!-- Optionally add helpers - button, thumbnail and/or media -->
-    <link rel="stylesheet" href="js/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
-    <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-    <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+    <link rel="stylesheet" href="/js/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="/js/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="/js/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
     
-    <link rel="stylesheet" href="js/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
-    <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+    <link rel="stylesheet" href="/js/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="/js/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
     
     <script type="text/javascript">
     	$(document).ready(function() {
