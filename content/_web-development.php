edd849793cf7e73e9b25462cdd9cffedec113c9d
Matt McNeil <matt.mcneil1984@gmail.com>
2017-10-27 20:39:52 +0000

    Finished website tweaks in other repo / added BretonSmartek site to Web Development page

--- content/_web-development.php
+++ content/_web-development.php
@@ -8,10 +8,10 @@
 <div id="container">					
 	<div id="row">
     <h1 class="text-center">Web Development</h1>
-    <!--<div class="col-xs-12 col-md-4">-->
-    <!--	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo1-bretonsmartek.jpg" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a>-->
-    <!--	<p class="text-center font-weight-bold">Breton SmarTek Logo</p>-->
-    <!--</div>-->
+    <div class="col-xs-12 col-md-4">
+    	<a class="thumbnail" href="bst_site/" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a>
+    	<p class="text-center font-weight-bold">Breton SmarTek / FireQ-RVS Website</p>
+    </div>
     <!--<div class="col-xs-12 col-md-4">-->
     <!--	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo2-fireq.jpg" alt="FireQ-RVS Logo"><img src="/images/graphic-design/Logo2-fireq.jpg" class="wide center-block"/></a>-->
     <!--	<p class="text-center font-weight-bold">FireQ-RVS Logo</p>-->
