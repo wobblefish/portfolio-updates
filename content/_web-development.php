d5ef2ccbd997be933cae1e7f56faa5b70446c9b3
Matt McNeil <matt.mcneil1984@gmail.com>
2017-10-29 03:14:04 +0000

    Updated web development styling/text + added link for website descriptions

--- content/_web-development.php
+++ content/_web-development.php
@@ -25,6 +25,10 @@
     padding: 30px;
   }
   
+  span{
+    font-size:small;
+    color: = #085a91;
+  }
 </style>
 <div id="container">					
 	<div id="row">
@@ -38,33 +42,39 @@
     <!--	<h3 class="text-center font-weight-bold">Breton SmarTek / FireQ-RVS Website</h3>-->
     <!--</div>-->
     
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12 col-md-6 text-center">
     	<a class="" href="#">
     	  <video id="BST" class="wide " autoplay loop muted poster="/images/web-development/BST-FS.png">
     	    <source type="video/mp4" src="/images/web-development/BST-FS.mp4">
     	    <source type="video/webm" src="/images/web-development/BST-FS.webm">
         </video>
       </a>
-    	<h3 class="text-center font-weight-bold">Mimir Networks Website</h3><span>(See Details)</span>
+    	  <h3 class="font-weight-bold">Breton SmarTek Website</h3>
+    	  <br>
+    	  <a href="#"><span>(See Details)</span></a>
     </div>
     
     <!--Make this into a fancybox window - play walkthrough video -->
-    <div class="col-xs-12 col-md-6">
+    <div class="col-xs-12 col-md-6 text-center">
     	<a class="fancybox fancybox" href="/images/web-development/MimirWalkthrough.mp4">
     	  <video id="Mimir" class="wide " autoplay loop muted poster="/images/web-development/Mimir-FS.png">
     	    <source type="video/mp4" src="/images/web-development/Mimir-FS.mp4">
     	    <source type="video/webm" src="/images/web-development/Mimir-FS.webm">
         </video>
       </a>
-    	<h3 class="text-center font-weight-bold">Mimir Networks Website</h3><span>(See Details)</span>
+    	  <h3 class="font-weight-bold">Mimir Networks Website</h3>
+    	  <br>
+    	  <a href="#"><span>(See Details)</span></a>
     </div>
     
     
     
     
-    <div class="col-xs-12 col-md-6" id="DevMatch">
+    <div class="col-xs-12 col-md-6 text-center" id="DevMatch">
       <a class="" href="https://devmatch120884.herokuapp.com/" alt="DevMatch"><img src="/images/web-development/DevMatch.png" /></a>
-      <h3 class="text-center font-weight-bold">DevMatch</h3><span>(See Details)</span>
+          <h3 class="font-weight-bold">DevMatch</h3>
+          <br>
+          <a href="#"><span>(See Details)</span></a>
     </div>
     
     
