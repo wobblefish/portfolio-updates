6b3b831f0ee2108d1a91ea1697c2c02ddd9694e4
Matt McNeil <matt.mcneil1984@gmail.com>
2017-10-30 22:53:34 +0000

    Updated Mimir Walkthrough video / styles for web development page and graphics page / added See Details modal with descriptions for each page which now opens in a new tab or mimir's video in fancybox

--- content/_graphics.php
+++ content/_graphics.php
@@ -8,48 +8,50 @@
 <div id="container">					
 	<div id="row">
     <h1 class="text-center">Logos</h1>
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo1-bretonsmartek.jpg" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a>
-    	<p class="text-center font-weight-bold">Breton SmarTek Logo</p>
-    </div>
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo2-fireq.jpg" alt="FireQ-RVS Logo"><img src="/images/graphic-design/Logo2-fireq.jpg" class="wide center-block"/></a>
-    	<p class="text-center font-weight-bold">FireQ-RVS Logo</p>
-    </div>
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo3-cit.png" alt="Customer In Touch Logo"><img src="/images/graphic-design/Logo3-cit.png" class="wide center-block"/></a>
-    	<p class="text-center font-weight-bold">Customer In Touch Logo</p>
-    </div>
     
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo4-ggm.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/Logo4-ggm.jpg" class="wide center-block" /></a>
-    	<p class="text-center font-weight-bold">Go Green Messaging Logo</p>
-    </div>
-    
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/businessCard.png" alt="Go Green Messaging Logo"><img src="/images/graphic-design/businessCard.png" class="wide center-block" /></a>
-    	<p class="text-center font-weight-bold">FireQ-RVS Business Cards</p>
-    </div>
-    
-    <div class="newline" style="height:20px; clear:both;"></div>
-    
-    <h1 class="text-center">Music Promotion Flyers</h1>
-    
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer1.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer1.jpg" class="tall center-block" /></a>
-    	<p class="text-center font-weight-bold">Electronic Funk 1</p>
-    </div>
-    
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer2.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer2.jpg" class="tall center-block" /></a>
-    	<p class="text-center font-weight-bold">Electronic Funk 2</p>
-    </div>
-    
-    <div class="col-xs-12 col-md-4">
-    	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer3.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer3.jpg" class="tall center-block" /></a>
-    	<p class="text-center font-weight-bold">Lucky 13</p>
-    </div>
-    
+      <div id="graphics">
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo1-bretonsmartek.jpg" alt="Breton SmarTek Logo"><img src="/images/graphic-design/Logo1-bretonsmartek.jpg" class="wide center-block"border="0" /></a>
+      	<p class="text-center font-weight-bold">Breton SmarTek Logo</p>
+      </div>
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo2-fireq.jpg" alt="FireQ-RVS Logo"><img src="/images/graphic-design/Logo2-fireq.jpg" class="wide center-block"/></a>
+      	<p class="text-center font-weight-bold">FireQ-RVS Logo</p>
+      </div>
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo3-cit.png" alt="Customer In Touch Logo"><img src="/images/graphic-design/Logo3-cit.png" class="wide center-block"/></a>
+      	<p class="text-center font-weight-bold">Customer In Touch Logo</p>
+      </div>
+      
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo4-ggm.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/Logo4-ggm.jpg" class="wide center-block" /></a>
+      	<p class="text-center font-weight-bold">Go Green Messaging Logo</p>
+      </div>
+      
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/businessCard.png" alt="Go Green Messaging Logo"><img src="/images/graphic-design/businessCard.png" class="wide center-block" /></a>
+      	<p class="text-center font-weight-bold">FireQ-RVS Business Cards</p>
+      </div>
+      
+      <div class="newline" style="height:20px; clear:both;"></div>
+      
+      <h1 class="text-center">Music Promotion Flyers</h1>
+      
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer1.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer1.jpg" class="tall center-block" /></a>
+      	<p class="text-center font-weight-bold">Electronic Funk 1</p>
+      </div>
+      
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer2.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer2.jpg" class="tall center-block" /></a>
+      	<p class="text-center font-weight-bold">Electronic Funk 2</p>
+      </div>
+      
+      <div class="col-xs-12 col-md-4">
+      	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/musicFlyer3.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/musicFlyer3.jpg" class="tall center-block" /></a>
+      	<p class="text-center font-weight-bold">Lucky 13</p>
+      </div>
+    </div>  
     <div class="spacer" style="height:60px; clear:both;"></div>
 	</div>
 </div>
--- content/_web-development.php
+++ content/_web-development.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="/js/h3opcorn-js-master/h3opcorn.js"></script>
+<script type="text/javascript" src="/js/popcorn-js-master/popcorn.js"></script>
 
 
 <script type="text/javascript">
@@ -10,96 +10,396 @@
 	});
 </script>
 
-<style>
-  img, video {
-    border: 3px solid hsla(0, 0%, 0%, 0.83);
-    box-shadow: -2px 2px 4px 1px rgb(20, 20, 20);
-    border-radius:40px;
-    padding:2px;
-    margin: 20px 0px;
-  }
-  #DevMatch img {
-    width: 100%;
-  }
-  .col-xs-12.col-md-6 {
-    padding: 30px;
-  }
-  
-  span{
-    font-size:small;
-    color: = #085a91;
-  }
-</style>
-<div id="container">					
+
+<div id="container" class="web-development">					
 	<div id="row">
     <h1 class="text-center">Web Development</h1>
-    <!--<div class="col-xs-12 col-md-6">-->
-    <!--	<a class="" href="bst_site/">-->
-    <!--	  <video id="BST-video" class="wide " autoplay loop muted poster="/images/web-development/BST-FS.png">-->
-    <!--	    <source type="video/mp4" src="/images/web-development/BST-FS.mp4">-->
-    <!--    </video>-->
-    <!--  </a>-->
-    <!--	<h3 class="text-center font-weight-bold">Breton SmarTek / FireQ-RVS Website</h3>-->
-    <!--</div>-->
-    
-    <div class="col-xs-12 col-md-6 text-center">
-    	<a class="" href="#">
-    	  <video id="BST" class="wide " autoplay loop muted poster="/images/web-development/BST-FS.png">
-    	    <source type="video/mp4" src="/images/web-development/BST-FS.mp4">
-    	    <source type="video/webm" src="/images/web-development/BST-FS.webm">
-        </video>
-      </a>
-    	  <h3 class="font-weight-bold">Breton SmarTek Website</h3>
-    	  <br>
-    	  <a href="#"><span>(See Details)</span></a>
+    <div id="web-development"> 
+      <div class="col-xs-12 col-md-4 col-md-offset-2 text-center">
+      	<a class="" href="bst_site" target="_blank" data-toggle="tooltip" data-placement="right" title="Visit Page">
+      	  <video id="BST" class="wide " autoplay loop muted poster="/images/web-development/BST-FS.png">
+      	    <source type="video/mp4" src="/images/web-development/BST-FS.mp4">
+      	    <source type="video/webm" src="/images/web-development/BST-FS.webm">
+          </video>
+        </a>
+      	  <h3 class="font-weight-bold">Breton SmarTek / FireQ-RVS Website</h3>
+      	  <br>
+      	  <a data-toggle="modal" data-target="#bretonsmartek-modal"><span>(See Details)</span></a>
+      </div>
+      
+      <!--Make this into a fancybox window - play walkthrough video -->
+      <div class="col-xs-12 col-md-6 text-center">
+      	<a class="fancybox fancybox.iframe" href="/images/web-development/MimirWalkthrough.mp4" data-toggle="tooltip" data-placement="right" title="Watch Demo">
+      	  <video id="Mimir" class="wide " autoplay loop muted poster="/images/web-development/Mimir-FS.png">
+      	    <source type="video/mp4" src="/images/web-development/Mimir-FS.mp4">
+      	    <source type="video/webm" src="/images/web-development/Mimir-FS.webm">
+          </video>
+        </a>
+      	  <h3 class="font-weight-bold">Mimir Networks Website</h3>
+      	  <br>
+      	  <a data-toggle="modal" data-target="#mimir-modal"><span>(See Details)</span></a>
+      </div>
+      
+      
+      
+      
+      <div class="col-xs-12 col-md-4 col-md-offset-2 text-center" id="DevMatch">
+        <a class="" href="https://devmatch120884.herokuapp.com/" alt="DevMatch" target="_blank" data-toggle="tooltip" data-placement="right" title="Visit Page"><img src="/images/web-development/DevMatch.png" /></a>
+            <h3 class="font-weight-bold">DevMatch</h3>
+            <br>
+            <a data-toggle="modal" data-target="#devmatch-modal"><span>(See Details)</span></a>
+      </div>
+      
+      <div class="col-xs-12 col-md-6 text-center"></div>
     </div>
-    
-    <!--Make this into a fancybox window - play walkthrough video -->
-    <div class="col-xs-12 col-md-6 text-center">
-    	<a class="fancybox fancybox" href="/images/web-development/MimirWalkthrough.mp4">
-    	  <video id="Mimir" class="wide " autoplay loop muted poster="/images/web-development/Mimir-FS.png">
-    	    <source type="video/mp4" src="/images/web-development/Mimir-FS.mp4">
-    	    <source type="video/webm" src="/images/web-development/Mimir-FS.webm">
-        </video>
-      </a>
-    	  <h3 class="font-weight-bold">Mimir Networks Website</h3>
-    	  <br>
-    	  <a href="#"><span>(See Details)</span></a>
-    </div>
-    
-    
-    
-    
-    <div class="col-xs-12 col-md-6 text-center" id="DevMatch">
-      <a class="" href="https://devmatch120884.herokuapp.com/" alt="DevMatch"><img src="/images/web-development/DevMatch.png" /></a>
-          <h3 class="font-weight-bold">DevMatch</h3>
-          <br>
-          <a href="#"><span>(See Details)</span></a>
-    </div>
-    
-    
-    
-    
-    
-    <!--<div class="col-xs-12 col-md-4">-->
-    <!--	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo3-cit.png" alt="Customer In Touch Logo"><img src="/images/graphic-design/Logo3-cit.png" class="wide "/></a>-->
-    <!--	<h3 class="text-center font-weight-bold">Customer In Touch Logo</h3>-->
-    <!--</div>-->
-    
-    <!--<div class="col-xs-12 col-md-4">-->
-    <!--	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/Logo4-ggm.jpg" alt="Go Green Messaging Logo"><img src="/images/graphic-design/Logo4-ggm.jpg" class="wide " /></a>-->
-    <!--	<h3 class="text-center font-weight-bold">Go Green Messaging Logo</h3>-->
-    <!--</div>-->
-    
-    <!--<div class="col-xs-12 col-md-4">-->
-    <!--	<a class="fancybox fancybox thumbnail" href="/images/graphic-design/businessCard.png" alt="Go Green Messaging Logo"><img src="/images/graphic-design/businessCard.png" class="wide " /></a>-->
-    <!--	<h3 class="text-center font-weight-bold">FireQ-RVS Business Cards</h3>-->
-    <!--</div>-->
-    
-    <!--<div class="newline" style="height:20px; clear:both;"></div>-->
-    
-    
-    
-    <!--<div class="spacer" style="height:60px; clear:both;"></div>-->
+
 	</div>
+</div>
+
+
+<!-- Modals -->
+<div id="bretonsmartek-modal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h1 class="modal-title">Breton SmarTek / FireQ-RVS</h1>
+      </div>
+      <div class="modal-body">
+        <div class="well">
+          <p>
+            Company and product web pages for sister US/Canadian companies offering
+        similar services. Serves different content based on location (    <em>Breton SmarTek for Canada / Thuh Company for US</em>)
+            <br/>
+            <br/>
+            <br/>
+        </p>
+        <p>
+            <a name="user-content-features"></a>
+            <strong>Features</strong>
+        </p>
+        <ul>
+            <li>
+                <p>
+                    Full company and product information and support pages with dynamic
+                    content (<em>ie images / contact form / text</em>)
+                </p>
+            </li>
+            <li>
+                <p>
+                    Several external JS libraries used to enhance user experience
+                </p>
+            </li>
+            <li>
+                <p>
+                    External API used to detect country by user’s IP address
+                </p>
+            </li>
+            <li>
+                <p>
+                    Search engine optimization
+                </p>
+            </li>
+            <li>
+                <p>
+                    Full training video series by me (FireQ-RVS Support section)
+                </p>
+            </li>
+        </ul>
+        <h1>
+            <a name="user-content-technologies-used"></a>
+            Technologies Used
+        </h1>
+        <p>
+            <strong>Languages / </strong>
+            <strong>Frameworks</strong>
+            <strong>:</strong>
+        </p>
+        <ul>
+            <li>
+                <p>
+                    HTML / CSS
+                </p>
+            </li>
+            <li>
+                <p>
+                    JavaScript / jQuery
+                </p>
+            </li>
+            <li>
+                <p>
+                    PHP
+                </p>
+            </li>
+        </ul>
+        <p>
+            <strong>Scripts / APIs / Tools Used:</strong>
+        </p>
+        <ul>
+            <li>
+                <p>
+                    MaxMind GeoIP API
+                    <sub>
+                        (subscription service used initially – added country switcher
+                        for demo purposes)
+                    </sub>
+                </p>
+            </li>
+            <li>
+                <p>
+                    TM Slider / Lean Slider
+                </p>
+            </li>
+            <li>
+                <p>
+                    FancyBox
+                </p>
+            </li>
+            <li>
+                <p>
+                    Google WebMaster Tools
+                </p>
+            </li>
+        </ul>
+    	  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<div id="mimir-modal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h1 class="modal-title">Mimir Networks Website</h1>
+      </div>
+      <div class="modal-body">
+        <div class="well">
+          <p>
+              Responsive company website developed in WordPress with company and product
+              information
+          </p>
+          <h1>
+              <a name="user-content-technologies-used"></a>
+              Technologies Used
+          </h1>
+          <p>
+              <strong>Languages / </strong>
+              <strong>Frameworks</strong>
+              <strong>:</strong>
+          </p>
+          <ul>
+              <li>
+                  <p>
+                      HTML / CSS
+                  </p>
+              </li>
+              <li>
+                  <p>
+                      WordPress
+                  </p>
+              </li>
+              <li>
+                  <p>
+                      PHP
+                  </p>
+              </li>
+          </ul>
+          <p>
+              <strong>WordPress Plugins</strong>
+              <strong> </strong>
+              <strong>Used</strong>
+              <strong>:</strong>
+          </p>
+          <ul>
+              <li>
+                  <p>
+                      Contact Form 7
+                  </p>
+              </li>
+              <li>
+                  <p>
+                      Theme Fusion Avada
+                  </p>
+              </li>
+              <li>
+                  <p>
+                      LayerSlider WP / Smart Slide 3 / Revolution Slider
+                  </p>
+              </li>
+          </ul>
+    	  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<div id="devmatch-modal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h1 class="modal-title">DevMatch</h1>
+      </div>
+      <div class="modal-body">
+        <div class="well">
+              <p>
+              	A full-stack Ruby On Rails application for a fictional startup company app
+              	called DevMatch. A membership-based networking platform for students,
+              	developers, entrepreneurs, and investors.
+              	<br/>
+              	<br/>
+              	<br/>
+              </p>
+              <p>
+              	<a name="user-content-features"></a>
+              	<strong>Features</strong>
+              </p>
+              <ul>
+              	<li>
+              		<p>
+              			A user interface
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Login/logout system with authentication
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Email notifications
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Subscription payments for premium services
+              		</p>
+              	</li>
+              </ul>
+              <h1>
+              	<a name="user-content-technologies-used"></a>
+              	Technologies Used
+              </h1>
+              <p>
+              	<strong>Languages:</strong>
+              </p>
+              <ul>
+              	<li>
+              		<p>
+              			HTML
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			CSS
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			JavaScript
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Ruby on Rails
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			PostgreSQL
+              		</p>
+              	</li>
+              </ul>
+              <p>
+              	<strong>Frameworks/Libaries:</strong>
+              </p>
+              <ul>
+              	<li>
+              		<p>
+              			BootStrap
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			SCSS
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			jQuery
+              		</p>
+              	</li>
+              </ul>
+              <p>
+              	<strong>Gems/APIs:</strong>
+              </p>
+              <ul>
+              	<li>
+              		<p>
+              			Figaro
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Devise (user profile setup)
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			PaperClip/ImageMagick (image upload handler)
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Stripe (subscription service)
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			MailGun (email sender)
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Hirb (database table formatting)
+              		</p>
+              	</li>
+              </ul>
+              <p>
+              	<strong>Tools:</strong>
+              </p>
+              <ul>
+              	<li>
+              		<p>
+              			Heroku (deployment)
+              		</p>
+              	</li>
+              	<li>
+              		<p>
+              			Cloud9 IDE
+              		</p>
+              	</li>
+              </ul>
+      	  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </div>
