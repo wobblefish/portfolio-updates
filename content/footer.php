66a8752a6f97b3b9260e5d3e1d44335fa4aab74c
Matt McNeil <matt.mcneil1984@gmail.com>
2017-11-05 17:32:36 +0000

    Footer HTML + CSS Tweaks

--- content/footer.php
+++ content/footer.php
@@ -5,18 +5,18 @@
 
 
 <div class="container">
-	<div class="footerButton">
+	<div class="footerButton text-center">
 		<button id="showHideFooter" type="button" class="btn btn-link"><i class="fa"></i></button>	
 	</div>
 	<footer class="footer">
 		<div class="row">
-			<div class="col-xs-11">
+			<div class="col-xs-10">
 				
 				<?php
 					echo $contactInfo;
 				?>
 			</div>
-			<div class="col-xs-1">
+			<div class="col-xs-2">
 				<div class="footerLinks">
 					<a href="//linkedin.com/in/matt-mcneil-1b356059/" data-toggle="tooltip" data-placement="top" title="LinkedIn Profile">
 						<i class="fa fa-linkedin-square" style="font-size:36px"></i>
